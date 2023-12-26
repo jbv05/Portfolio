dd5acccd2a4e9b4f76b49ddbd6942059d678fbe4
jbv05 <jjerobravovasc@gmail.com>
2023-12-26 18:39:25 -0500

    clase 9

---
+++ clase 9/main.js
@@ -0,0 +1,7 @@
+function openTab(tabName) {
+    const tabs = document.getElementsByClassName('tab');
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('active');
+    }
+    document.getElementById(tabName).classList.add('active');
+}
